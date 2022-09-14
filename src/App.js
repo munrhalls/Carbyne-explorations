aa194767607d18a6bcf1e20d86a77c70f2f39b73
John <antarcticdepths@gmail.com>
2022-09-14 15:38:56 +0200

    blog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-// design ideas
-// dials into?
-
 import "./App.css";
+import TopMace from "./TopMace/TopMace";
+import Content from "./Content/Content";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <TopMace />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/TopMace/TopMace.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TopMace() {
+  return <div className="TopMace">TopMace</div>;
+}
