e76576d3216ce0d03f2c5e35516b30da9509b64d
John <antarcticdepths@gmail.com>
2022-09-14 15:56:39 +0200

    blog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+// globals
+import useWindowHeight from "./Hooks/useWindowHeight";
+// app
 import "./App.css";
 import TopMace from "./components/TopMace/TopMace";
 import Content from "./components/Content/Content";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ height: `${useWindowHeight()}px` }}>
       <TopMace />
       <Content />
     </div>
