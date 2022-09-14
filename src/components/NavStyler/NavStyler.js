e4710447e4c3fab4bf9001752c832397a7578eee
John <antarcticdepths@gmail.com>
2022-09-14 16:42:53 +0200

    nav styler

--- src/components/NavStyler/NavStyler.js
+++ src/components/NavStyler/NavStyler.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
+import useWindowHeight from "./../../Hooks/useWindowHeight";
 
 export default function NavStyler() {
+  const vh = useWindowHeight();
+  const topDistances = [...Array(vh).keys()].map((n) => (n += 5));
+
   return (
     <div className="NavStyler">
-      {Array(5)
+      {Array(vh)
         .fill(null)
-        .map((n) => {
+        .map((n, index) => {
           return (
-            <div className="NavStyler__line" key={uuid()}>
-              line
-            </div>
+            <div
+              className="NavStyler__line"
+              key={uuid()}
+              style={{ top: `${topDistances[index]}px` }}
+            ></div>
           );
         })}
     </div>
