7e6bc47b25225526226499c43dccc57293b8d35e
John <antarcticdepths@gmail.com>
2022-09-14 16:32:26 +0200

    v4

--- src/components/NavStyler/NavStyler.js
+++ src/components/NavStyler/NavStyler.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 
 export default function NavStyler() {
-  return <div className="NavStyler">NavStyler</div>;
+  return (
+    <div className="NavStyler">
+      {Array(5)
+        .fill(null)
+        .map((n) => {
+          return (
+            <div className="NavStyler__line" key={uuid()}>
+              line
+            </div>
+          );
+        })}
+    </div>
+  );
 }
