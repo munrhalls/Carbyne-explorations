a59989acec98d76189ff673e56fa8b1a1b24134e
John <antarcticdepths@gmail.com>
2022-09-15 08:17:46 +0200

    nav styler

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import useWindowHeight from "./Hooks/useWindowHeight";
 // app
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import NavStyler from "./components/NavStyler/NavStyler";
 import Content from "./components/Content/Content";
 
 function App() {
   return (
     <div className="App" style={{ height: `${useWindowHeight()}px` }}>
       <Nav />
+      <NavStyler />
       <Content />
     </div>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,9 +2,5 @@ import React from "react";
 import NavStyler from "../NavStyler/NavStyler";
 
 export default function Nav() {
-  return (
-    <div className="Nav">
-      <NavStyler />
-    </div>
-  );
+  return <div className="Nav"></div>;
 }
