23920b7eee7b1ffd3cf863138dec69e0d60da63f
John <antarcticdepths@gmail.com>
2022-09-15 17:45:00 +0200

    stylers

--- src/Animators/ScannerAnim.js
+++ src/Animators/ScannerAnim.js
@@ -1,12 +1,29 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import useWindowHeight from "../Hooks/useWindowHeight";
+import useInterval from "../Hooks/useInterval";
 
 export default function ScannerAnim() {
-  const scannerVerticalPoints = Array(useWindowHeight()).fill();
+  let [count, setCount] = useState(0);
+  const vh = useWindowHeight();
+
+  useInterval(() => {
+    setCount(count + 1);
+  }, 0.1);
+
+  useEffect(() => {
+    if (count * 10 > vh) setCount(0);
+  });
 
   return (
     <div className="ScannerAnim">
-      <div className="ScannerAnim__line" style={{ top: `${250}px` }}></div>
+      <div
+        className="ScannerAnim__line"
+        style={{ top: `${count * 10}px` }}
+      ></div>
+      <div
+        className="ScannerAnim__line"
+        style={{ top: `${vh - count * 10}px` }}
+      ></div>
     </div>
   );
 }
--- src/components/ScannerLoader/ScannerLoader.js
+++ src/components/ScannerLoader/ScannerLoader.js
@@ -1,17 +1,12 @@
 import React from "react";
-import StylerLines from "../../Stylers/StylerLines/StylerLines";
 // animators
 import ScannerAnim from "../../Animators/ScannerAnim";
 
 export default function ScannerLoader({ children, active }) {
-
-  
-
   return (
     <>
       {active ? (
         <div className="ScannerLoader">
-          <StylerLines distanceBetweenLines={0.5} />
           <ScannerAnim />
         </div>
       ) : (
