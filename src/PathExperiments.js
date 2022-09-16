802fa00d08100a579f378c3c5e6a83ef008242d8
John <antarcticdepths@gmail.com>
2022-09-16 20:31:49 +0200

    svg

---
+++ src/PathExperiments.js
@@ -0,0 +1,87 @@
+// modif a bit = symbols, iconographic-like!!!
+
+{
+  /* <path
+        d={`M ${width / 2},${height / 2} 
+        L${width / 2 + 0} , ${height / 2 + 100}
+        L${width / 2 + 100} , ${height / 2 + 0}
+        L${width / 2 + 0} , ${height / 2 - 100}
+        L${width / 2 - 100} , ${height / 2 - 0}`}
+        fill="none"
+        stroke="black"
+        strokeWidth="5" */
+}
+
+{
+  /* <path
+        d={`M ${width / 2},${height / 2} 
+        L${width / 2 + 0} , ${height / 2 + 100}
+        L${width / 2 + 100} , ${height / 2 + 100}
+        L${width / 2 + 100} , ${height / 2 - 100}
+        L${width / 2 - 100} , ${height / 2 - 100}`}
+        fill="none"
+        stroke="black"
+        strokeWidth="5"
+      /> */
+}
+
+// <path
+//         d={`M ${width / 2},${height / 2}
+
+//         L${width / 2 + 100} , ${height / 2 + 100}
+//         L${width / 2} , ${height / 2 + 100}
+//         L${width / 2 + 100} , ${height / 2 + 100}
+//         L${width / 2 + 100} , ${height / 2}
+//         `}
+//         fill="none"
+//         stroke="black"
+//         strokeWidth="5"
+//       />
+
+{
+  /* <path
+        d={`M ${width / 2},${height / 2} 
+
+
+        Q  ${width / 2 + 50} , ${height / 2 - 50} 
+          ${width / 2 + 100} , ${height / 2}
+
+        Q  ${width / 2 - 50} , ${height / 2 + 50} 
+          ${width / 2} , ${height / 2}
+          
+        L${width / 2 + 100} , ${height / 2 + 100}
+        L${width / 2} , ${height / 2 + 100}
+        L${width / 2} , ${height / 2}
+
+        
+        
+        
+        `}
+        fill="none"
+        stroke="black"
+        strokeWidth="5"
+      />
+
+       */
+}
+
+// <path
+//         d={`M ${width / 2},${height / 2}
+
+//         Q  ${width / 2 + 50} , ${height / 2 - 50}
+//           ${width / 2 + 100} , ${height / 2}
+
+//         M ${width / 2},${height / 2}
+
+//         Q  ${width / 2 + 50} , ${height / 2 + 50}
+//           ${width / 2 + 100} , ${height / 2}
+
+//         L${width / 2 + 100} , ${height / 2 + 100}
+//         L${width / 2} , ${height / 2 + 100}
+//         L${width / 2} , ${height / 2}
+
+//         `}
+//         fill="none"
+//         stroke="black"
+//         strokeWidth="5"
+//       />
--- src/Stylers/Blanket/Blanket.js
+++ src/Stylers/Blanket/Blanket.js
@@ -7,28 +7,56 @@ export default function Blanket() {
   const width = String(useWindowWidth());
   console.log(width);
 
-  const svg = (
+  // const svg = (
+  //   <svg
+  //     className="Blanket"
+  //     version="1.1"
+  //     width={width}
+  //     height={height}
+  //     xmlns="http://www.w3.org/2000/svg"
+  //     xmlnsXlink="http://www.w3.org/1999/xlink"
+  //   >
+  //     {/* <rect width="100%" height="100%" fill="blue" />
+  //     <circle cx="150" cy="100" r="80" fill="black" />
+  //     <text x="150" y="125" fontSize="60" textAnchor="middle" fill="white">
+  //       SVG
+  //     </text> */}
+  //     <path
+  //       d={`M ${width}, ${height / 10}
+  //       L ${width - 100}, ${height / 10}
+
+  //       `}
+  //       fill="none"
+  //       stroke="black"
+  //       strokeWidth="5"
+  //     />
+  //   </svg>
+  // );
+
+  return (
     <svg
       className="Blanket"
       version="1.1"
-      width={width}
-      height={height}
+      height={String(height)}
+      width={String(width)}
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
       {/* <rect width="100%" height="100%" fill="blue" />
-      <circle cx="150" cy="100" r="80" fill="black" />
-      <text x="150" y="125" fontSize="60" textAnchor="middle" fill="white">
-        SVG
-      </text> */}
+  <circle cx="150" cy="100" r="80" fill="black" />
+  <text x="150" y="125" fontSize="60" textAnchor="middle" fill="white">
+    SVG
+  </text> */}
       <path
-        d={`M0,0 L${width}, ${height} Q120,0 25,120 T77, 205`}
+        d={`M ${width}, ${height / 10}
+    L ${width * 0.3}, ${height / 10}
+
+
+    `}
         fill="none"
         stroke="black"
         strokeWidth="5"
       />
     </svg>
   );
-
-  return svg;
 }
