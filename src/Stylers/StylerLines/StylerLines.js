fe92d2ee027bdc4e59edb98002ba2d66cae22c78
John <antarcticdepths@gmail.com>
2022-10-04 10:18:18 +0200

    refactor

--- src/Stylers/StylerLines/StylerLines.js
+++ src/Stylers/StylerLines/StylerLines.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect, useRef } from "react";
-import { v4 as uuid } from "uuid";
-import useWindowHeight from "../../Hooks/useWindowHeight";
+// import React, { useState, useEffect, useRef } from "react";
+// import { v4 as uuid } from "uuid";
+// import useWindowHeight from "../../Hooks/useWindowHeight";
 
-export default function StylerLines({ distanceBetweenLines, highlightProp }) {
-  const vhPixelsNum = useWindowHeight();
+// export default function StylerLines({ distanceBetweenLines, highlightProp }) {
+//   const vhPixelsNum = useWindowHeight();
 
-  // vertical
-  const distanceMultiBetweenHorizLines = distanceBetweenLines || 1.5;
-  const iterationsFloat = vhPixelsNum / distanceMultiBetweenHorizLines;
-  const iterationsNBasedOnViewportHeight = Math.floor(iterationsFloat);
+//   // vertical
+//   const distanceMultiBetweenHorizLines = distanceBetweenLines || 1.5;
+//   const iterationsFloat = vhPixelsNum / distanceMultiBetweenHorizLines;
+//   const iterationsNBasedOnViewportHeight = Math.floor(iterationsFloat);
 
-  const progressiveVerticalPointsListBasedOnViewportHeight = [
-    ...Array(iterationsNBasedOnViewportHeight).keys(),
-  ].map((n) => n * distanceMultiBetweenHorizLines);
+//   const progressiveVerticalPointsListBasedOnViewportHeight = [
+//     ...Array(iterationsNBasedOnViewportHeight).keys(),
+//   ].map((n) => n * distanceMultiBetweenHorizLines);
 
-  // horiz
+//   // horiz
 
-  return (
-    <>
-      {Array(iterationsNBasedOnViewportHeight)
-        .fill(null)
-        .map((n, index) => {
-          return (
-            <div
-              className="StylerLines"
-              key={uuid()}
-              style={{
-                top: `${progressiveVerticalPointsListBasedOnViewportHeight[index]}px`,
-                right: `${progressiveVerticalPointsListBasedOnViewportHeight[index]}px`,
-              }}
-            ></div>
-          );
-        })}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {Array(iterationsNBasedOnViewportHeight)
+//         .fill(null)
+//         .map((n, index) => {
+//           return (
+//             <div
+//               className="StylerLines"
+//               key={uuid()}
+//               style={{
+//                 top: `${progressiveVerticalPointsListBasedOnViewportHeight[index]}px`,
+//                 right: `${progressiveVerticalPointsListBasedOnViewportHeight[index]}px`,
+//               }}
+//             ></div>
+//           );
+//         })}
+//     </>
+//   );
+// }
