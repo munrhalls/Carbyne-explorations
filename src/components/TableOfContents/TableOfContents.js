e93fe7e74fe9ba72ce4d5060ee4e1f78e9308bc4
John <antarcticdepths@gmail.com>
2022-10-07 16:52:32 +0200

    observer works

--- src/components/TableOfContents/TableOfContents.js
+++ src/components/TableOfContents/TableOfContents.js
@@ -5,11 +5,45 @@ import { useGlobContext } from "../../Contexts/GlobProvider";
 export default function TableOfContents({ items }) {
   const { globColor } = useGlobContext();
 
+  useEffect(() => {
+    let entries = document.querySelectorAll("[data-scroll]");
+
+    let options = {
+      root: document.getElementsByClassName(".ContentMananger")[0],
+      rootMargin: "0px",
+      threshold: 0.25,
+    };
+
+    console.log(entries);
+
+    const io = new IntersectionObserver((entries, options) => {
+      entries.forEach((entry) => {
+        console.log("OBSERVING!!!");
+        if (entry.intersectionRatio > 0) {
+          // Add 'active' class if observation target is inside viewport
+          console.log("!!!!!!!!!!!!!!!!!");
+          entry.target.classList.add("active");
+        } else {
+          // Remove 'active' class otherwise
+          entry.target.classList.remove("active");
+        }
+      });
+    });
+    entries.forEach((entry) => {
+      io.observe(entry);
+    });
+    return () => {
+      entries.forEach((entry) => {
+        io.unobserve(entry);
+      });
+    };
+  }, [items]);
+
   function scrollTo(i) {
     const item = document.querySelector(`[data-scroll="${i}"]`);
     item.scrollIntoView({ behavior: "smooth" });
   }
-  console.log(items);
+
   return (
     <div
       style={{ border: `3px solid ${globColor}` }}
