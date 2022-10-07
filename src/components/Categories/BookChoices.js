fe1f94dce27ec5efd06005c061b1c6d225717490
John <antarcticdepths@gmail.com>
2022-10-07 17:52:50 +0200

    workflow skills

--- src/components/Categories/BookChoices.js
+++ src/components/Categories/BookChoices.js
@@ -1,23 +1,28 @@
 import React from "react";
+import TableOfContents from "../TableOfContents/TableOfContents";
 
 export default function BookChoices() {
   return (
-    <div className="Category">
-      <div data-scroll="0" className="Category__item">
-        <h1>#1Josh Waitzkin, The Art of Learning</h1>
-        <p>
-          “Growth comes at the point of resistance. We learn by pushing
-          ourselves and finding what really lies at the outer reaches of our
-          abilities.” — Josh Waitzkin
-        </p>
-        <p>Content coming...</p>
-      </div>
+    <>
+      <TableOfContents items={["#1Josh Waitzkin, The Art of Learning"]} />
 
-      <div className="Content__segregatorLine"></div>
+      <div className="Category">
+        <div data-scroll="0" className="Category__item">
+          <h1>#1Josh Waitzkin, The Art of Learning</h1>
+          <p>
+            “Growth comes at the point of resistance. We learn by pushing
+            ourselves and finding what really lies at the outer reaches of our
+            abilities.” — Josh Waitzkin
+          </p>
+          <p>Content coming...</p>
+        </div>
 
-      <div data-scroll="1" className="Category__item">
-        <h1></h1>
+        <div className="Content__segregatorLine"></div>
+
+        <div data-scroll="1" className="Category__item">
+          <h1></h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
