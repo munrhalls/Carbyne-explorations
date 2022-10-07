b9033db5c09f44847978567d3331e4b3db5083a6
John <antarcticdepths@gmail.com>
2022-10-07 17:16:00 +0200

    to refactor

--- src/components/TableOfContents/TableOfContents.js
+++ src/components/TableOfContents/TableOfContents.js
@@ -5,39 +5,56 @@ import { useGlobContext } from "../../Contexts/GlobProvider";
 export default function TableOfContents({ items }) {
   const { globColor } = useGlobContext();
 
-  useEffect(() => {
-    let entries = document.querySelectorAll("[data-scroll]");
+  // useEffect(() => {
+  //   let entries = document.querySelectorAll("[data-scroll]");
+  //   console.log(entries);
 
-    let options = {
-      root: document.getElementsByClassName(".ContentMananger")[0],
-      rootMargin: "0px",
-      threshold: 0.25,
-    };
+  //   const options = {
+  //     root: document.getElementsByClassName(".ContentMananger")[0],
+  //     rootMargin: "0px",
+  //     threshold: 0,
+  //   };
 
-    console.log(entries);
+  //   console.log(entries);
 
-    const io = new IntersectionObserver((entries, options) => {
-      entries.forEach((entry) => {
-        console.log("OBSERVING!!!");
-        if (entry.intersectionRatio > 0) {
-          // Add 'active' class if observation target is inside viewport
-          console.log("!!!!!!!!!!!!!!!!!");
-          entry.target.classList.add("active");
-        } else {
-          // Remove 'active' class otherwise
-          entry.target.classList.remove("active");
-        }
-      });
-    });
-    entries.forEach((entry) => {
-      io.observe(entry);
-    });
-    return () => {
-      entries.forEach((entry) => {
-        io.unobserve(entry);
-      });
-    };
-  }, [items]);
+  //   const io = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry, index) => {
+  //       console.log("OBSERVING!!!");
+  //       const item = document.getElementsByClassName(
+  //         "TableOfContents__list__item"
+  //       )[index];
+
+  //       if (entry.intersectionRatio > 0.4) {
+  //         // Add 'active' class if observation target is inside viewport
+  //         console.log("!!!!!!!!!!!!!!!!!");
+
+  //         console.log(item, index);
+  //         item.classList.add("--scrollActive");
+  //       } else {
+  //         // Remove 'active' class otherwise
+
+  //         console.log(item, index);
+  //         item.classList.remove("--scrollActive");
+  //       }
+  //     });
+  //   });
+  //   io.options = options;
+
+  //   entries.forEach((entry) => {
+  //     io.observe(entry);
+  //   });
+
+  //   return () => {
+  //     entries.forEach((entry, index) => {
+  //       const item = document.getElementsByClassName(
+  //         "TableOfContents__list__item"
+  //       )[index];
+  //       console.log(item, index);
+  //       item.classList.remove("--scrollActive");
+  //       io.unobserve(entry);
+  //     });
+  //   };
+  // }, [items]);
 
   function scrollTo(i) {
     const item = document.querySelector(`[data-scroll="${i}"]`);
