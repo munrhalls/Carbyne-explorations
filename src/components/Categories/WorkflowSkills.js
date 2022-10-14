d42df2e8e5e75fd225e9dba99308d59e01bd84f7
John <antarcticdepths@gmail.com>
2022-10-15 00:05:13 +0200

    working skills

--- src/components/Categories/WorkflowSkills.js
+++ src/components/Categories/WorkflowSkills.js
@@ -1,7 +1,11 @@
 import React from "react";
 import TableOfContents from "../TableOfContents/TableOfContents";
+import styled from "@emotion/styled";
 
 export default function WorkflowSkills() {
+  const P = styled.p`
+    margin-top: 0.75rem;
+  `;
   return (
     <>
       <TableOfContents items={["Take your time in a hurry"]} />
@@ -9,25 +13,47 @@ export default function WorkflowSkills() {
       <div className="Category">
         <div data-scroll="0" className="Category__item">
           <h1>Take your time in a hurry</h1>
-          <p>
+          <P>
             "Fast is fine, but accuracy is everything. In a gun fight... You
             need to take your time in a hurry." - Wyatt Earp
-          </p>
-          <p>
+          </P>
+          <P>
             Speed management is different from time management. Applying this
             difference is definitely an advantage you want.
-          </p>
-          <p>
+          </P>
+          <P>
             Working too slow, stagnancy and lack of clarity cripples in. Working
-            too quickly, it's spray and pray all over the place. Steadfast doesn't work either.
-          </p>
-          <p>
+            too quickly, it's spray and pray all over the place. Steadfast
+            doesn't work either.
+          </P>
+          <P>
             All of these items can and do work...sometimes. Other times, they
             lead to miserable stuttering, rushing or ineffective rigidity.
-          </p>
+          </P>
           <h2>The problem</h2>
-          <p>Under time pressure, we want to complete that next task FAST. This is rarely a benign desire, we really, really want it FAST. Job security, releasing the threat of being late, looking all competent and skillful, respect, practical ease, time, money...the line of incentives is virtually endless.</p>
-          <p> </p>
+          <P>
+            Under time pressure, we want to complete that next task FAST. This
+            is rarely a benign desire, we really, really want it FAST. Job
+            security, releasing the threat of being late, looking all competent
+            and skillful, respect, practical ease, time, money...the line of
+            incentives is not short.
+          </P>
+          <P>
+            In fact, it is long enough to completely overpower common sense not
+            once or twice, but nearly indefinitely. More so, this need for speed
+            is so compelling, it completely conceals itself, too. It becomes the
+            unrecognized influence.
+          </P>
+          <P>
+            This is not a desirable influence. Actually, it's a type of a
+            hard-to-notice tendency that frequently derails and delays, while
+            being seen as pre-requirement to stay on track. It creates illusory
+            speed and, simulatenously, decelerates real speed.
+          </P>
+          <P>
+            What is that subtle, derailing tendency, exactly? More importantly,
+            what can one practice to become better at spotting it?
+          </P>
         </div>
 
         <div className="Content__segregatorLine"></div>
