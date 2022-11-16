31a7895224b767c54671de9e68b8ce1f2c21326b
John <antarcticdepths@gmail.com>
2022-11-16 16:55:18 +0100

    intro

--- src/components/Categories/WorkflowSkills.js
+++ src/components/Categories/WorkflowSkills.js
@@ -53,6 +53,109 @@ export default function WorkflowSkills() {
             effectiveness and efficiency ratio, as availably possible in the
             current moment.{" "}
           </h3>
+          <p>
+            In practice, this is all about making solving work problems easier.
+          </p>
+          <p>
+            I'm stuck at X. Y is going too slow. Z is just repeatedly
+            unreliable, just can't do it without freaking errors. When workflow
+            skills are a bag of items that make it that much easier and simpler
+            to solve such problems, then workflow skills fulfill their purpose.
+          </p>
+          <p>
+            Remember R. Dalio's book, Principles (Book choices segment)?
+            Workflow skills are about having a functional mechanism (machine)
+            already ready, for solving types of problems, instead of solving
+            each problem with work effectiveness individually, when it announces
+            itself by knocking your efforts out to the sad and irrelevant
+            garbage can, with its iron fist of
+            I-tried-so-hard-and-it-didn't-even-matter and I-don't-know-why
+            ineffectiveness.
+          </p>
+          <h3>
+            Wait, wait, wait...this is just an introduction and it's already
+            getting insanely theoretical, what the heck's going on in here?
+          </h3>
+
+          <p>
+            It's best summarized as "how to be effective and efficient in
+            general".
+          </p>
+          <p>
+            If you've read some of the Aristotle’s Metaphysics, he has a very
+            curious observation in that for every concept, there are universals
+            and particulars.
+          </p>
+          <p>
+            I'm likely paraphrasing but for the needs of this introduction,
+            universals are aspects of a concept that are present and influential
+            in every possible instance of that concept.{" "}
+          </p>
+          <p>
+            Particulars, are aspects of a concept that are present only in a
+            specific instance or type of instances of that same concept.
+          </p>
+          <h3>What is this nonsense?</h3>
+          <p>
+            I completely understand how it can look to some people - if you
+            don't feel curious, I encourage you to skip directly to the tools I
+            explore after the introduction.
+          </p>
+          <p>
+            What's going on in this introduction is about what enables building
+            effective workflow skills.{" "}
+          </p>
+          <p>
+            What's after the introduction is about exploring the workflow skills
+            I've built so far or am currently building.
+          </p>
+          <h3>Distinguihing universals from particulars.</h3>
+          <p>
+            Let's say you have a concept of order. The particulars, there are
+            aspects of order that are present only in some types of order, e.g.
+            Japanesse way of organizing your room etc. The universals, these are
+            the features that are unconditionally present in any kind of order
+            whatsoever, whether it's an ant colony, a human language, a computer
+            language, a teenager's room, a cosmic spacecraft, a real thing, a
+            fantasy story, anything whatsoever - if you talk about order, these
+            features are always present no matter what. This is what universals
+            are and they are fascinating because while abstract and hard to
+            translate, they tend to be extremely practical due to their
+            encompassing nature.{" "}
+          </p>
+          <p>
+            The same distinction - between features that are universals and
+            features that are particulars - exist for every concept you can
+            think of, whether it be money, politics, strategy, power, love,
+            planet or in our case, the concept of efficiency + effectiveness.
+          </p>
+          <p>
+            Most interestingly, however, the particulars are most often first
+            shaped and determined, and made possible in the first place, by the
+            universals. They're just "different clothes" and "add-ons" of what
+            the essence of the concept actually is.
+          </p>
+          <h3>
+            This entire segment is a playful (I hope) exploration and study of
+            universals for the concept of efficiency and effectiveness (as in,
+            efficiency + effectiveness in general, unconditionally === what?)
+          </h3>
+          <p>
+            It's not organized to be a philosophical treatise, though. It's
+            organized to revolve around direct, real-world applicability and
+            relevance of use.
+          </p>
+          <p>
+            Essentially, I want this segment to about "I have a workflow problem
+            X, how can I stop having workflow problem X" or "Y is ineffective,
+            how do I make Y effective" or "Z is slower than damn tortoise, how
+            do I make Z fast" or "C is more unreliable than a teenager's room
+            order, how do I make C reliable".
+          </p>
+          <p>
+            Collection of recipes that, with possibly minimal adaptations,
+            really do make it easier solve such problems.
+          </p>
         </div>
         <div data-scroll="1" className="Category__item">
           <h1>Take your time in a hurry</h1>
