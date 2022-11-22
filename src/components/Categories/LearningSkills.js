41102983c9b600a097490daf525c83b8d9178894
John <antarcticdepths@gmail.com>
2022-11-22 11:23:31 +0100

    learning intro

--- src/components/Categories/LearningSkills.js
+++ src/components/Categories/LearningSkills.js
@@ -5,8 +5,216 @@ export default function LearningSkills() {
     <div className="Category">
       <div className="Category">
         <div data-scroll="0" className="Category__item">
-          <h1>learning skills</h1>
-          <p>Content coming...</p>
+          <h1>Introduction - dealing with BIG MIS-CONCEPTIONS</h1>
+          <p>
+            You cannot succesfully manage a long-term learning endeavor if your
+            approach is stained with strategically false ideas that you perceive
+            as true ideas.
+          </p>
+          <p>
+            Likewise, you cannot escape operating from out of a set of flawed
+            ideas, from within those ideas. You need to step outside of those
+            ideas.{" "}
+          </p>
+          <h2>
+            Gargantuan mis-conception: working in relation to time, instead of
+            goals.
+          </h2>
+          <p>
+            Many people underscore how developing a complex, refined skillset
+            takes time. How one needs to look at putting in the work every day,
+            the results will eventually come. Progress will occur. Just keep
+            showing up regularly and doing your best.
+          </p>
+          <p>NO, NO, NO AND NO.</p>
+          <p>That is NOT TRUE.</p>
+          <p>
+            This is an example of an idea sligthly twisted - not very twisted,
+            just slightly. A cognitive bias that is easy to fall for, and very
+            hard to spot. Especially, given that it is reinforced by the
+            collective effect of such a cognitive bias - many people believe
+            that, promote that and treat it as "obvious".
+          </p>
+          <h2>
+            The cognitive bias: it's huge, it takes long time, therefore focus
+            on time input and regularity, instead of goals.
+          </h2>
+          <p>This bias takes many forms.</p>
+          <p>
+            Just keep the long-term goal clear but daily, focus on work
+            input.Or, just keep improving your training cycles and reps, focus
+            on the system, not goal. Or, if you just focus on the end-goal,
+            you'll be frustrated, you need to just put in the time every day.
+          </p>
+          <p>
+            All of these ideas are flawed and stem from an imprecise thought
+            process. They are inaccurate and like flawed GPS, they lead astray,
+            delay progress hugely and impede proper feedback cycles.
+          </p>
+          <h2>
+            The "correct move" isn't to abandon goals-focus, just because a
+            skillset is huge and takes many months and years to develop.
+          </h2>
+          <p>
+            When a skillset to internalize is of huge proportions, the right
+            move is to keep identifying the next few, small steps. These steps
+            are real goals. They can be failed or succeeded. They aren't "I'm
+            going to study or traing or repeat Y for X amount of time."
+          </p>
+          <p>
+            X amount of time IS NOT RELEVANT. It's just a doorway to very sublte
+            and consequently, very hard to notice, self-delusion. Joining in
+            with the great crowds who share a s cognitive bias.
+          </p>
+          <p>
+            When the world was flat (it never was), people believed it because
+            the social effect of everyone else around treating that as an
+            obvious fact, was very imposing and compelling.
+          </p>
+          <p>
+            Of course, reality is larger than whatever a group of entities on
+            some planet happens to believe at some window of time...but it
+            became a social effect or commonly shared assumption because it was
+            an assumption that was very hard not to be made - it seemed so.
+          </p>
+          <p>
+            With big, cosmological questions, that isn't necessarily a huge deal
+            for an individual. However, it is a huge deal when it comes to
+            personal efficiency and effectiveness, especially in learning, and
+            especially in learning a skillset that's large and takes a long time
+            to develop.
+          </p>
+          <h2>
+            When a skillset is very large, takes very long to develop, how can
+            you work in relation to goals?
+          </h2>
+          <p>
+            I remember listening to the podcast of Tim Ferriss with Dave Elitch,
+            the very famous drummer and drum instructor. Mr. Dave explained how
+            some students work in relation to time.
+          </p>
+          <p>
+            "I showed up today, I showed up every day this month and the
+            previous month, I did my absolute best for 8-10 hours of intense
+            training, I'm good, I'm on my way."
+          </p>
+          <p>No, a person like this isn't on the way to anywhere.</p>
+          <p>
+            Even the very statement itself, involves blur, lack of clarity, as
+            if just putting in a ton of more-or-less concentrated effort equaled
+            good progress.
+          </p>
+          <p>
+            Dave Elitch explained how if you want to be really good, you need to
+            work in relation to goals, not time. If it's a drumming piece, you
+            need to de-construct it into component building blocks and master a
+            small block, one at a time. You also want to master it in a way that
+            you can play at random. E.g., you can play the last 10 seconds of
+            the song, no problem because your *way of training* enabled that.{" "}
+          </p>
+          <p>
+            You can easily hear a lot of opposition when you put it that way,
+            which is charming and funny, to say the least. That it's still the
+            same as putting in a lot of time, time-boxing and so on.
+          </p>
+          <p>
+            Only with an order of ideas that are like a teenager's room, can one
+            mix such two distinct concepts together.
+          </p>
+          <h2>
+            Okay but how do you do it, then? You want to train to become great
+            at X, you have 10 hours today, what do you do?
+          </h2>
+          <p>
+            It's not a big formula or a big technique. It's not about that. It's
+            simply identifying the next few steps, instead of the next X amount
+            of time.
+          </p>
+          <p>
+            If you set a goal like, I'm going to study language vocab for 2
+            hours, that's instantly a blur, it's not clear and therefore, it's
+            misleading. The only truly visible, sure thing about it, is that an
+            approach like this fails/
+          </p>
+          <p>
+            If you set a goal like, I'm gonna learn these 5 vocab pieces, then
+            these 5, then these 5, and then using them at random, now that's
+            much clearer. It's easy to say, for each of these items, whether
+            they've been completed or not. It's easy to say, whether it's going
+            well or not. It's just much clearer and therfore, easier to navigate
+            and keep it real.
+          </p>
+          <p>
+            The fundamental problem with "I'm going to work on this for X amount
+            of time" is that it is easy to not keep it real. Just because one
+            works for X amount of time, doesn't mean equivalent X amount of real
+            stuff is being completed. It also doesn't mean that X amount of
+            potential is equivalently utilized and applied in the best easily
+            available, common sense manner. What it means, is weaving some
+            unclear hogwire and coaxing perception to see the good in it.
+          </p>
+          <h2>
+            Alright but how can you possibly work in relation to goals, when the
+            end-goal is so far away, and not be awfully frustrated?
+          </h2>
+          <p>
+            This entire conversation is like going into a teenager's room and
+            re-ordering a lot of things that were mixed up together in all kinds
+            of wrong ways.
+          </p>
+          <p>
+            You can work in relation to goals, on the technical side. Always
+            ideanting next few, real small steps. Always in the manner of 1-2
+            small themes at a time but always keeping it real. Steps need to be
+            real. They need to give real feedback
+          </p>
+          <p>
+            X progrss point, be it a set of vocab terms in a language, or a
+            bench press target, or the ability to de-construct a problem of
+            n-level visual complexity, it either is achieved or not. It doesn't
+            matter what tasks were done or how much time they took. It matters
+            what was actually achieved from out of it all. That's real feedback
+            and not deluding yourself.
+          </p>
+          <p>
+            This the technical side. It's real, when it is set up in a manner
+            such as this. Notice how it maximizes requirements to keep changing
+            and adapting. You cannot escape gross mis-match between intended
+            result and result, if a gross mis-match exists. It doesn't matter
+            that you worked for 8 hours or tried your hardest or did 50 tasks.
+            These are all means, not ends. So, the only course left, isn't to
+            try harder, or to put in more hours, or more tasks/ The only course
+            left, is to change and keep changing, to keep actually climbing the
+            next few steps up. That's real, on the technical side.{" "}
+          </p>
+          <p>On the psychological side...</p>
+          <p>
+            That's where you need to take all these ideas about time-boxing,
+            doing X for Y amount of time, doing X amount of tasks per Y amount
+            of time, all that jazz, and appreciate yourself for trying.
+          </p>
+          <p>
+            You see, instead of deceiving yourself that X blur
+            "should-be-progress" is progress (it isn't progress, it's blur), you
+            remain distinctly aware of how X blur is a failure. It's a ton of
+            time and energy, emotion and thought, all into a thing that didn't
+            quite work - that's precisely where the fork in the road is. You
+            either deceive yourself or you tell yourself the truth with no
+            judgement - you did 50 tasks, none of them worked. That's alright.
+          </p>
+          <p>
+            The real thing here, is to shift the idea that time or tasks or
+            amount of effort matters. What matters is feedback and changing what
+            small steps you set for yourself.
+          </p>
+          <p>
+            Small steps need to always be real, small goals that follow certain
+            characteristics of a well-set goal.
+          </p>
+          <h2>
+            Instead of tricking oneself with blur-ideas, stay with clear ideas
+            and use self-appreciation.
+          </h2>
         </div>
 
         <div className="Content__segregatorLine"></div>
